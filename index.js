19daae02aeb99bed6023bb68fb4abeff3f93ce7e
Ken Liu <ken.liu@thenetcircle.com>
2017-07-19 15:55:14 +0800

    finished all basic components of druid query; adjust api interface and how internal objects  are exposed to the outside

--- index.js
+++ index.js
@@ -3,17 +3,19 @@
 var Druid = require('./druid'),
     utils = require('./lib/utils'),
     queries = utils.moduleMap(__dirname + '/lib/query'),
-    filter = require('./lib/filter/filter'),
-    aggregator = require('./lib/aggregator/aggregator'),
+    aggregators = utils.moduleMap(__dirname + '/lib/aggregator'),
+    filters = utils.moduleMap(__dirname + '/lib/filter'),
+    posts = utils.moduleMap(__dirname + '/lib/post'),
+    having = utils.moduleMap(__dirname + '/lib/having'),
     each = require('lodash/each')
 
 module.exports = Druid
 
 each(queries, (query) =>{
-    Druid[query.name] =  query
+    Druid[query.name] =  (ds) => new query(ds)
 })
 
-Druid['filter'] = filter
-Druid['aggregator'] = aggregator
+
+
 
 
